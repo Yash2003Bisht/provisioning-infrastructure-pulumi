4c9522bf1869d9cff289b840553e8499ad1c1f37
yash2003bisht <yash2003bisht@gmail.com>
2023-04-30 17:42:39 +0530

    debug mode added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from source import create_app
+from source import create_app, debug
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5000)
+    app.run("0.0.0.0", port=5000, debug=debug)
