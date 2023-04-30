6e20f2d772476a95256beb7ae94c1c64e4e52555
yash2003bisht <yash2003bisht@gmail.com>
2023-04-30 17:43:55 +0530

    user & virtual machine models created

---
+++ source/models.py
@@ -0,0 +1,18 @@
+from source import database
+from flask_login import UserMixin
+
+
+class User(database.Model, UserMixin):
+    id = database.Column(database.Integer, primary_key=True)
+    name =  database.Column(database.String(300))
+    email = database.Column(database.String(300), unique=True)
+    password = database.Column(database.String(300))
+    virtual_machines = database.relationship("VirtualMachines")
+
+
+class VirtualMachines(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    name = database.Column(database.String(500), unique=True)
+    dns_name = database.Column(database.String(500))
+    console_url = database.Column(database.String(500))
+    refrence_key = database.Column(database.Integer, database.ForeignKey("user.id"))
