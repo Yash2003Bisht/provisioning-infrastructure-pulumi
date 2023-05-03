fff0d2e547bea48a867dcd3528020828061d45b8
yash2003bisht <yash2003bisht@gmail.com>
2023-05-03 13:19:00 +0530

    sites model created

--- source/models.py
+++ source/models.py
@@ -8,11 +8,20 @@ class User(database.Model, UserMixin):
     email = database.Column(database.String(300), unique=True)
     password = database.Column(database.String(300))
     virtual_machines = database.relationship("VirtualMachines")
+    sites = database.relationship("Sites")
 
 
 class VirtualMachines(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String(500), unique=True)
     dns_name = database.Column(database.String(500))
+    console_url = database.Column(database.String(500))
+    refrence_key = database.Column(database.Integer, database.ForeignKey("user.id"))
+
+
+class Sites(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    name = database.Column(database.String(500), unique=True)
+    url = database.Column(database.String(500))
     console_url = database.Column(database.String(500))
     refrence_key = database.Column(database.Integer, database.ForeignKey("user.id"))
